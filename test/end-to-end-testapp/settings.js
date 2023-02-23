432ffdff39a4256c1234f67fbb997ea848e4f537
VinodKumarWingify <vinod.kumar@wingify.com>
2023-02-23 15:42:41 +0530

    Updated initialization.js file with settings value

--- test/end-to-end-testapp/settings.js
+++ test/end-to-end-testapp/settings.js
@@ -1,6 +1,12 @@
 var SDKsettings = {
     apiKey: 'testAPIKey',
-    account_id: 632691//654331 // prod account
+    account_id: 0, // Mandatory field
+    version: 1.4,
+    settings_tolerance: 2000,
+    library_tolerance: 2500,
+    use_existing_jquery: false,
+    is_spa: 1,
+    hide_element: 'body',
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
     into the src/initialization.js file as the
